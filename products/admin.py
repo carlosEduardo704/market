ee3f49c0352ea53f5f4dee99f5f1bb4a6576772e
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-10-05 23:55:23 -0300

    register models 'Product' and 'Department'

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from products.models import Department, Product
 
 # Register your models here.
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'department', 'price', 'bar_code')
+    search_fields = ('name', 'department', 'bar_code')
+
+
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Product, ProductAdmin)
