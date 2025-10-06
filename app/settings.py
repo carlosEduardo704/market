bd471686bb805bd49fda2828e666bf08897213fb
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-10-05 23:26:37 -0300

    set mysql as default database

--- app/settings.py
+++ app/settings.py
@@ -74,8 +74,15 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'market',
+        'USER': 'carlos',
+        'PASSWORD': 'C@du2761',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'sql_mode': 'traditional'
+        }
     }
 }
 
