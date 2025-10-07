53ac40875042d7254cee4f0e29558a4ec202633c
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-10-06 23:19:19 -0300

    create views 'HomePageView' and 'ProductListView'

--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from products.models import Product
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 # Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products_list.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        products = super().get_queryset().order_by('name')
+        search = self.request.GET.get('search')
+
+        if search:
+            products =products.filter(name__icontains=search)
+
+        return products
