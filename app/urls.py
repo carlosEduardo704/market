fcbcb27b539ed1e4795d78f0480a9e4fe79872c0
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-10-06 23:17:31 -0300

    adding paths 'home_page' and 'products_list' plus config media static

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from products.views import HomePageView, ProductListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', HomePageView.as_view(), name='home_page'),
+    path('products/', ProductListView.as_view(), name='products_list')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
