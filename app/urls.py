075f1973fd8459a5ff549bf2326d04277b1e735c
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-10-12 10:20:17 -0300

    add RegisterView to path register

--- app/urls.py
+++ app/urls.py
@@ -1,32 +1,22 @@
-"""
-URL configuration for app project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+
+# Views and imports from products
 from products.views import HomePageView, ProductListView
-from accounts.views import register_view
+
+# Views and imports from accounts
+from accounts.views import RegisterView
 from django.contrib.auth import views as auth_views
+from accounts.forms import CustomAuthenticationForm
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home_page'),
-    path('register/', register_view, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html', form_class=CustomAuthenticationForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('products/', ProductListView.as_view(), name='products_list')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
