298bab25f79fec24ab86c8fabae22464ad88dd46
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-10-20 00:35:41 -0300

    fix forms.py

--- products/forms.py
+++ products/forms.py
@@ -3,5 +3,6 @@ from products.models import Product
 
 
 class ProductModelForm(forms.ModelForm):
-    model = Product
-    fields = '__all__'
+    class Meta:
+        model = Product
+        fields = '__all__'
