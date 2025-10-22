3f6f562e7a2854e1c9bee41b9bfdb956851249a9
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-10-22 09:06:35 -0300

    update product_detail url

--- app/urls.py
+++ app/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html', form_class=CustomAuthenticationForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('products/', ProductListView.as_view(), name='products_list'),
-    path('product/<int:pk>/', ProducDetailView.as_view(), name='product_detail'),
+    path('product/<int:pk>/<str:bar_code>/', ProducDetailView.as_view(), name='product_detail'),
     path('new_product/', ProductCreateView.as_view(), name='new_product')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
