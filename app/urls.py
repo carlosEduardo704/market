9a234c6ad424494ef56a63f5566145e950ab21c6
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-10-23 08:44:04 -0300

    add include to urls

--- app/urls.py
+++ app/urls.py
@@ -1,26 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path, include
 
-# Views and imports from products
+from django.conf.urls.static import static, settings
+from django.views.generic import TemplateView
 from products.models import Product
-from products.views import ProductListView, ProducDetailView, ProductCreateView
-from django.views.generic import TemplateView, DetailView
-
-# Views and imports from accounts
-from accounts.views import RegisterView
-from django.contrib.auth import views as auth_views
-from accounts.forms import CustomAuthenticationForm
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html', extra_context={'products': Product.objects.all()}), name='home_page'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html', form_class=CustomAuthenticationForm), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
-    path('products/', ProductListView.as_view(), name='products_list'),
-    path('product/<int:pk>/<str:bar_code>/', ProducDetailView.as_view(), name='product_detail'),
-    path('new_product/', ProductCreateView.as_view(), name='new_product')
+    path('', include('accounts.urls')),
+    path('', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
