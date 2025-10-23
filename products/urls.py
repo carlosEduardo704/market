66743bae39fd7bcceed66a431d3d656363a96b18
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-10-23 08:44:44 -0300

    create urls file

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from products.views import ProductListView, ProducDetailView, ProductCreateView
+from django.views.generic import TemplateView, DetailView
+
+urlpatterns = [
+    path('products/', ProductListView.as_view(), name='products_list'),
+    path('product/<int:pk>/<str:bar_code>/', ProducDetailView.as_view(), name='product_detail'),
+    path('new_product/', ProductCreateView.as_view(), name='new_product')
+]
