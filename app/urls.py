3a7bf8185d8d6781e3279069018f1b28caf01b0e
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-10-23 00:22:27 -0300

    add context to home TemplateView

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 # Views and imports from products
+from products.models import Product
 from products.views import ProductListView, ProducDetailView, ProductCreateView
 from django.views.generic import TemplateView, DetailView
 
@@ -15,7 +16,7 @@ from accounts.forms import CustomAuthenticationForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home_page'),
+    path('', TemplateView.as_view(template_name='home.html', extra_context={'products': Product.objects.all()}), name='home_page'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html', form_class=CustomAuthenticationForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
