6b44fa27dca53b5825dd1dc4b6ab164a03a255ac
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-11-01 00:22:43 -0300

    alter fileds Product.name and Department.name update max_length

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Department(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
@@ -12,7 +12,7 @@ class Department(models.Model):
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
     bar_code = models.CharField(max_length=13, unique=True)
     price = models.FloatField(blank=True, null=True)
     department = models.ForeignKey(Department, on_delete=models.PROTECT, related_name='product_department')
