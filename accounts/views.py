5a1b6a7aba0eecc2dc55b1243d85b11fd926c4bd
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-12-09 09:45:19 -0300

    add AccountDetailView

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from .forms import CustomUserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from order.models import Order, OrderItem
 
 # Create your views here.
 class RegisterView(CreateView):
@@ -8,3 +10,16 @@ class RegisterView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
 
+class AccountDatailView(LoginRequiredMixin, TemplateView):
+    template_name = 'my_account.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        orders = []
+        for order in Order.objects.filter(user=self.request.user):
+            orders.append(order)
+
+        context['orders'] = orders
+
+        return context
