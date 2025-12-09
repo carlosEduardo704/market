8c622047d25a129f14083413ae1c4f57f562b73b
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-12-09 09:39:30 -0300

    add 'user' field to Order model

--- order/models.py
+++ order/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from cart.models import CartItem
 from products.models import Product
+from django.contrib.auth import settings
 
 class Order(models.Model):
     PAYMENT_METHODS = [
@@ -9,7 +10,7 @@ class Order(models.Model):
         ('CREDIT_CARD', 'Cartão de Crédito'),
         ('DEBIT_CARD', 'Cartão de Débito')
     ]
-
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     payment_method = models.CharField(max_length=20, choices=PAYMENT_METHODS, default='TICKET')
     total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
