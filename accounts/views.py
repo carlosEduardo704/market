eaf01028e9e5c6f2cb790a1f5a0b7d2fd11baaae
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-12-19 13:14:17 -0300

    rename OrdersView, create OrderDetailView and AdressDetailView

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from .forms import CustomUserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from order.models import Order, OrderItem
+from accounts.mixins import RestrictUserMixin
 
 # Create your views here.
 class RegisterView(CreateView):
@@ -11,15 +12,33 @@ class RegisterView(CreateView):
     success_url = reverse_lazy('login')
 
 class AccountDatailView(LoginRequiredMixin, TemplateView):
-    template_name = 'my_account.html'
+    template_name = 'account_information.html'
+
+
+class OrdersView(LoginRequiredMixin, TemplateView):
+    model = Order
+    template_name = 'orders.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
-        orders = []
-        for order in Order.objects.filter(user=self.request.user):
-            orders.append(order)
-
+        orders = [order for order in Order.objects.filter(user=self.request.user)]
         context['orders'] = orders
 
         return context
+
+
+class OrderDetailView(RestrictUserMixin, LoginRequiredMixin, DetailView):
+    model = Order
+    template_name = 'order_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        order_itens = [item for item in OrderItem.objects.filter(order=self.kwargs['pk'])]
+        context['items'] = order_itens
+        
+        return context
+
+class AdressDetailView(LoginRequiredMixin, TemplateView):
+    template_name = 'adresses.html'
