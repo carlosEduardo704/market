0eba74da1107f3d47dda3450c1a21597ecbfecbb
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-12-22 09:07:03 -0300

    create clean() function to limiter user address and setting labels

--- address/forms.py
+++ address/forms.py
@@ -1,11 +1,23 @@
 from django import forms
-from address.models import Address, MAX_ADDRESSES_PER_USER
+from django.core.exceptions import ValidationError
+from address.models import Address
 
+MAX_ADDRESSES_PER_USER = 3
 
 class AddressModelForm(forms.ModelForm):
     class Meta:
         model = Address
-        fields = "__all__"
+        exclude = ['user']
+
+        labels = {
+            'adress_name': 'Nome',
+            'street': 'Rua',
+            'number': 'Número',
+            'zip_code': 'CEP',
+            'city': 'Cidade',
+            'district': 'Municipio',
+            'uf': 'Estado'
+        }
 
         widgets = {
             "user": forms.HiddenInput(),
@@ -13,9 +25,27 @@ class AddressModelForm(forms.ModelForm):
             "updated_at": forms.HiddenInput()
         }
 
+    def __init__(self, *args, user=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = user
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+
+        if not self.instance.pk and self.user:
+            if Address.objects.filter(user=self.user).count() >= MAX_ADDRESSES_PER_USER:
+                raise ValidationError(f"Um usuário não pode ter mais de {MAX_ADDRESSES_PER_USER} endereços cadastrados!")
+        
+        return cleaned_data
+
     def clean_user(self):
         user = self.cleaned_data.get("user")
         if not self.instance.pk:
             if Address.objects.filter(user=user).count() >= MAX_ADDRESSES_PER_USER:
                 raise forms.ValidationError(f"Usuário não pode ter mais de {MAX_ADDRESSES_PER_USER} endereços cadastrados!")
         return user
+    
+    error_messages = {
+
+    }
