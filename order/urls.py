50fcb8ae763b9f90f0c40228504e6fe691519e21
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-12-22 09:14:47 -0300

    transfering orders and orders detail urls to order.urls.py file

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from order.views import CheckoutFormView
+from order.views import CheckoutFormView, OrdersView, OrderDetailView
 
 
 urlpatterns = [
     path('checkout/', CheckoutFormView.as_view(), name='checkout'),
+    path('orders/', OrdersView.as_view(), name='orders'),
+    path('orders/<int:pk>', OrderDetailView.as_view(), name='order_detail'),
 ]
