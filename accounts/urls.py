623e2c5d4c65af12fe91cc6059afad0623c46521
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-12-22 09:00:26 -0300

    cleaning code

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from accounts.views import RegisterView, AccountDatailView, OrdersView, AdressDetailView, OrderDetailView
+from accounts.views import RegisterView, AccountDatailView
 from django.contrib.auth import views as auth_views
 from accounts.forms import CustomAuthenticationForm
 
@@ -9,7 +9,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html', form_class=CustomAuthenticationForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('my_account/', AccountDatailView.as_view(), name='my_account'),
-    path('orders/', OrdersView.as_view(), name='orders'),
-    path('orders/<int:pk>', OrderDetailView.as_view(), name='order_detail'),
-    path('adresses', AdressDetailView.as_view(), name='adress_detail')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,7 @@
 from .forms import CustomUserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView, DetailView
+from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from order.models import Order, OrderItem
-from accounts.mixins import RestrictUserMixin
 
 # Create your views here.
 class RegisterView(CreateView):
@@ -13,32 +11,3 @@ class RegisterView(CreateView):
 
 class AccountDatailView(LoginRequiredMixin, TemplateView):
     template_name = 'account_information.html'
-
-
-class OrdersView(LoginRequiredMixin, TemplateView):
-    model = Order
-    template_name = 'orders.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        
-        orders = [order for order in Order.objects.filter(user=self.request.user)]
-        context['orders'] = orders
-
-        return context
-
-
-class OrderDetailView(RestrictUserMixin, LoginRequiredMixin, DetailView):
-    model = Order
-    template_name = 'order_detail.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        order_itens = [item for item in OrderItem.objects.filter(order=self.kwargs['pk'])]
-        context['items'] = order_itens
-        
-        return context
-
-class AdressDetailView(LoginRequiredMixin, TemplateView):
-    template_name = 'adresses.html'
