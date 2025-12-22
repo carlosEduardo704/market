eced2f173b49a1a42a75a0786edc4646c6b0766b
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-12-22 09:05:29 -0300

    remove clean() function

--- address/models.py
+++ address/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 from django.core.exceptions import ValidationError
-from django.conf import settings
+from django.contrib.auth.models import User
 from localflavor.br.models import BRStateField, BRPostalCodeField
 
-MAX_ADDRESSES_PER_USER = 3
-
 class Address(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_adress')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_adress')
     adress_name = models.CharField(max_length=20)
     street = models.CharField(max_length=50)
     number = models.CharField(max_length=6)
@@ -17,15 +15,10 @@ class Address(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
-    def clean(self):
-        if not self.pk:
-            if Adress.objects.filter(user=self.user).count() >= MAX_ADDRESSES_PER_USER:
-                raise ValidationError(f"Um usuário não pode ter mais de {MAX_ADDRESSES_PER_USER} endereços cadastrados!")
-
+        
     def save(self, *args, **kwargs):
         self.full_clean()
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.adress_name}, {self.street}, {self.number}, {self.city}"
+        return f"{self.adress_name}, {self.street}, {self.number}, {self.zip_code}, {self.city}, {self.district}, {self.uf}"
