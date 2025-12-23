3ef7ca88ac997cdaeeb3b830ce55f3c73a04afd8
carlosEduardo704 <carlos704eduardo@gmail.com>
2025-12-23 09:00:43 -0300

    add url to delete address

--- address/urls.py
+++ address/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from address.views import AddressListView
+from address.views import AddressListView, RemoveAddressDeleteView
 
 
 urlpatterns = [
     path('addresses/', AddressListView.as_view(), name='adress_list'),
+    path('addresses/<int:pk>/delete/', RemoveAddressDeleteView.as_view(), name='delete_address')
 ]
