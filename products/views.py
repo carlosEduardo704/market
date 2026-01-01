fad3d6e3f36c6c76a84847cf20b9882efb3acf57
carlosEduardo704 <carlos704eduardo@gmail.com>
2026-01-01 15:47:29 -0300

    remove unused comments

--- products/views.py
+++ products/views.py
@@ -13,11 +13,6 @@ class ProductListView(ListView):
     template_name = 'products_list.html'
     context_object_name = 'products'
 
-    # def get_queryset(self):
-    #     queryset = super().get_queryset().filter(department=True)
-
-    #     return queryset
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
