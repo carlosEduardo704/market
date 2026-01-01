f4f60519ca9d636916acdc1325d6e783430845fa
carlosEduardo704 <carlos704eduardo@gmail.com>
2026-01-01 00:52:57 -0300

    'add get_or_create' on cart to solve errros in tests

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -3,7 +3,8 @@ from cart.models import Cart
 
 def get_total_from_cart(request):
     if request.user.is_authenticated:
-        total_itens = Cart.objects.get(usuario=request.user).get_total_itens
+        cart, created = Cart.objects.get_or_create(usuario=request.user)
+        total_itens = cart.get_total_itens
 
         return {
             'cart_total_itens': total_itens
