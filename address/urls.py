b52ff0ae937dc3410cd8b412bc6b338d37658c3e
carlosEduardo704 <carlos704eduardo@gmail.com>
2026-01-01 15:44:48 -0300

    add 'create_addres and update_address' urls

--- address/urls.py
+++ address/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from address.views import AddressListView, RemoveAddressDeleteView
+from address.views import AddressListView, RemoveAddressDeleteView, CreateAdressView, UpdateAddressView
 
 
 urlpatterns = [
     path('addresses/', AddressListView.as_view(), name='adress_list'),
-    path('addresses/<int:pk>/delete/', RemoveAddressDeleteView.as_view(), name='delete_address')
+    path('addresses/<int:pk>/delete/', RemoveAddressDeleteView.as_view(), name='delete_address'),
+    path('addresses/create_address', CreateAdressView.as_view(), name='create_address'),
+    path('addresses/update_address/<int:pk>', UpdateAddressView.as_view(), name='update_address')
 ]
