b4652c128102eea68dbcfc7e6bc2fe518ae10cb6
carlosEduardo704 <carlos704eduardo@gmail.com>
2026-01-01 15:45:50 -0300

    create views CreateAddress and UpdateAddress

--- address/views.py
+++ address/views.py
@@ -1,26 +1,13 @@
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from address.models import Address
 from address.forms import AddressModelForm
 from django.urls import reverse_lazy
 
 
-class AddressListView(CreateView):
+class AddressListView(ListView):
     model = Address
     template_name = 'address.html'
-    form_class = AddressModelForm
-    success_url = reverse_lazy('adress_list')
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['user'] = self.request.user
-        return kwargs
-
-        
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super().form_valid(form)
-
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -29,6 +16,35 @@ class AddressListView(CreateView):
         context['addresses'] = addresses
         
         return context
+
+
+class CreateAdressView(CreateView):
+    model = Address
+    template_name = 'create_address.html'
+    form_class = AddressModelForm
+    success_url = reverse_lazy('adress_list')
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class UpdateAddressView(UpdateView, LoginRequiredMixin, PermissionRequiredMixin):
+    model = Address
+    template_name = 'update_address.html'
+    context_object_name = 'addresses'
+    permission_required = ('addresses.change_Address')
+    form_class = AddressModelForm
+    success_url = reverse_lazy('adress_list')
+    
+    # Restringe o queryset para que o usuário não possa deletar o endereço de outro usuário.
+    def get_queryset(self):
+        return Address.objects.filter(user=self.request.user)
 
 
 class RemoveAddressDeleteView(LoginRequiredMixin, DeleteView):
