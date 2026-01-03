662cdac163f5b5c680f44a874575a5c15040d57a
carlosEduardo704 <carlos704eduardo@gmail.com>
2026-01-03 09:29:26 -0300

    setting form to address in checkout page

--- order/forms.py
+++ order/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from order.models import Order
-
+from address.models import Address
 
 class OrderModelForm(forms.ModelForm):
-    
+    address = forms.ModelChoiceField(
+        widget=forms.RadioSelect,
+        queryset=Address.objects.none(),
+        empty_label=None,
+        label='Escolha um endereço'
+    )
+
     class Meta:
         model = Order
         fields = ['payment_method']
@@ -11,3 +17,8 @@ class OrderModelForm(forms.ModelForm):
         widgets = {
             'payment_method': forms.Select
         }
+
+    # filtra o queryset para que só mostre os endereços do usuário
+    def __init__(self, *args, user=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["address"].queryset = Address.objects.filter(user=user)
