5896f1e9f671f2d5e9eaecb89aa01f22a7a1f9e6
carlosEduardo704 <carlos704eduardo@gmail.com>
2026-01-23 10:37:33 -0300

    add 'first_name', 'last_name' fields and make them required

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,10 +3,12 @@ from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
 
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
     email = forms.EmailField(required=True, help_text='Required. Enter a valid email address.')
 
     class Meta(UserCreationForm.Meta):
-        fields = UserCreationForm.Meta.fields + ('email',)
+        fields = UserCreationForm.Meta.fields + ('email', 'first_name', 'last_name')
 
 
 class CustomAuthenticationForm(AuthenticationForm):
