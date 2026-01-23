f030d13ef284228567a098eef8058a7a5fc03583
carlosEduardo704 <carlos704eduardo@gmail.com>
2026-01-23 15:56:06 -0300

    create UserUpdateView

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, UserUpdateForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 
 # Create your views here.
 class RegisterView(CreateView):
@@ -11,3 +12,12 @@ class RegisterView(CreateView):
 
 class AccountDatailView(LoginRequiredMixin, TemplateView):
     template_name = 'account_information.html'
+
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = 'account_update_information.html'
+    form_class = UserUpdateForm
+    success_url = reverse_lazy('my_account')
+
+    def get_object(self, queryset=None):
+        return self.request.user
